31063f3a98edc4ddfdf36d4e5d79f4eb0471bd7f
Kevin McDonald <github@sudorandom.com>
2025-02-13 19:49:23 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+
+var handler = new GrpcWebHandler(new HttpClientHandler());
+
+using var channel = GrpcChannel.ForAddress("http://127.0.0.1:6660", new GrpcChannelOptions { HttpHandler = handler });
+
+var client = new Connectrpc.Eliza.V1.ElizaService.ElizaServiceClient(channel);
+var request = new Connectrpc.Eliza.V1.SayRequest { Sentence = "World from .NET" };
+Console.WriteLine(request.Sentence);
+var reply = await client.SayAsync(request);
+Console.WriteLine(reply.Sentence);
+
